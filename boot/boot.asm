24b732fd956dc4a0bc5c93db0581765ab18f9660
DevyDhanish <dhanishkariyatmain@gmail.com>
2024-02-02 20:12:04 +0530

    Gonna work on Kernal

--- boot/boot.asm
+++ boot/boot.asm
@@ -7,11 +7,24 @@ org 0x7c00
 mov bx, KEY
 call puts
 
-hlt
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
 
-;
-;   FUNCTIONS
-;
+cli
+lgdt [gdt_descriptor]
+mov eax, cr0
+mov eax, 0x1
+mov cr0, eax
+jmp CODE_SEG:proc_mode
+
+
+PREV_DL: db 0x0
+PREV_DH: db 0x8
+KEY: db "Booting Done - Welcome to Meow Operating System", 0xa, 0x0
+DISK_READ_ERROR: db "Failed to read the disk", 0xa, 0x0
+PROC_MODE_REACHED : db "Your in 32-bit proctected mode", 0xa, 0x0
+
+
 
 newline:
     mov ah, 0x2
@@ -35,20 +48,51 @@ write_char:
     ret
 
 puts:
-    pusha
     mov ah, 0x0e
     call write_char
-    popa
     ret
 
 ;
-;   DATA/VARIABLES
+;
+;   GDT
 ;
 
-PREV_DL: db 0x0
-PREV_DH: db 0x8
-KEY: db "Booting Done - Welcome to Meow Operating System", 0xa, 0x0
-DISK_READ_ERROR: db "Failed to read the disk", 0xa, 0x0
+
+gdt_start:
+
+    gdt_null:
+        dd 0x0
+        dd 0x0
+    
+    gdt_code:
+        dw 0xfffff  ; use the full 4gb
+        dw 0x0      ;
+        db 0x0
+        db 10011010b
+        db 11001111b
+        db 0x0
+    
+    gdt_data:
+        dw 0xfffff  ; use the full 4gb
+        dw 0x0      ;
+        db 0x0
+        db 10010010b
+        db 11001111b
+        db 0x0
+
+gdt_end:
+
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1
+    dd gdt_start
+
+[bits 32]
+proc_mode:
+    mov al, 'A'
+    mov ah, 0x0f
+    mov [0xb8000], ax
+    jmp $
+
 
 times 510 - ($-$$) db 0
 dw 0xaa55
